f4fe47140d5301f799e39c898009474ad13ef85e
Jesse <RaistlinD2x.dev@outlook.com>
2021-05-17 11:16:11 -0500

    created basic html and added click event to generate button

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'angular-password-generator';
+  onButtonClick() {
+    console.log('button was clicked')
+  }
 }
