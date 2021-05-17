b5037f6d7f92d753e85fc830eb7909bd1d4963ab
Jesse <RaistlinD2x.dev@outlook.com>
2021-05-17 13:22:30 -0500

    added any cast to onChangeLength function and assigned generate output to "Your Password"

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,10 +6,20 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  length = 0;
   password = '';
   includeLetters = false;
   includeNumbers = false;
   includeSymbols = false;
+
+  
+  onChangeLength(value: string) {
+    const parsedValue = parseInt(value);
+
+    if (!isNaN(parsedValue)) {
+      this.length = parsedValue;
+    }
+  }
 
   onChangeUseLetters() {
     this.includeLetters = !this.includeLetters
@@ -24,9 +34,26 @@ export class AppComponent {
   }
 
   onButtonClick() {
-    this.password = 'thisismynewpassword'
-    console.log(this.includeLetters)
-    console.log(this.includeNumbers)
-    console.log(this.includeSymbols)
+    const numbers = '1234567890'
+    const letters = 'abcdefghijklmnopqrstuvwxyz'
+    const symbols = '!@#$%^&*()'
+
+    let validChars = ''
+    if (this.includeLetters) {
+      validChars += letters;
+    }
+    if (this.includeNumbers) {
+      validChars += numbers;
+    }
+    if (this.includeSymbols) {
+      validChars += symbols
+    }
+
+    let generatedPassword = '';
+    for (let i = 0; i < this.length; i++) {
+      const index = Math.floor(Math.random() * validChars.length);
+      generatedPassword += validChars[index];
+    }
+    this.password = generatedPassword;
   }
 }
